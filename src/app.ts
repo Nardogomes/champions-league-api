c469a87fe2ceecc1f8842028b54d792f168fab7d
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-08 21:55:45 -0300

    feat: separate the server from the app layer

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express"
+
+export function createApp() {
+  const app = express()
+  
+  app.use(express.json())
+  
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("hello world")
+  })
+
+  return app
+}

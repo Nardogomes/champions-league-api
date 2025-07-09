12570ac9b291e46286986e0669a29e7ac9bc5963
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-08 22:06:45 -0300

    feat: create the controller layer

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express, { Request, Response } from "express"
+import express from "express"
+import { getPlayer } from "./controllers/players-controller"
 
 export function createApp() {
   const app = express()
   
   app.use(express.json())
   
-  app.get("/", (req: Request, res: Response) => {
-    res.status(200).send("hello world")
-  })
+  app.get("/", getPlayer)
 
   return app
 }

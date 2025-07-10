fb43736c072072f40a7875103de6f43a939edaaa
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-09 22:02:26 -0300

    feat: create repositories layer

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,3 +1,16 @@
+import * as findAllPlayers from "../repositories/players-repository"
+import { ok, noContent } from "../utils/http-helper"
+
 export const getPlayerService = async () => {
-  return { player: "Jogador #1" }
+  const data = await findAllPlayers.findAllPlayers()
+
+  let response = null
+
+  if(data) {
+    response = await ok(data)
+  } else {
+    response = await noContent()
+  }
+
+  return response
 }

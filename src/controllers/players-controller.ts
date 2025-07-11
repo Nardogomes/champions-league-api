246b005ddd013aa676ab1422af104c413f79e3c0
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-11 14:34:27 -0300

    feat: implement player registration fuction.

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import * as PlayerService from "../services/players-service"
+import { noContent } from "../utils/http-helper"
 
 export const getPlayer = async (req: Request, res: Response) => {
   const httpResponse = await PlayerService.getPlayerService()
@@ -13,4 +14,17 @@ export const getPlayerById = async (req: Request, res: Response) => {
   const httpResponse = await PlayerService.getPlayerByIdService(id)
 
   res.status(httpResponse.statusCode).json(httpResponse.body)
+}
+
+export const postPlayer = async (req: Request, res: Response) => {
+  const bodyValue = req.body
+
+  const httpResponse = await PlayerService.createPlayerService(bodyValue)
+
+  if(httpResponse) {
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  } else {
+    const response = await noContent()
+    res.status(response.statusCode).json(response.body)
+  }
 }
